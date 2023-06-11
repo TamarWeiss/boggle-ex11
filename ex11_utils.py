c8b908a9066fc715bf93ace74ffd893c144c522c
Tamar Weiss <tamarweiss1292014@gmail.com>
2023-06-11 11:40:15 +0300

    Initial commit

---
+++ ex11_utils.py
@@ -0,0 +1,16 @@
+from typing import Iterable, Optional
+
+Board = list[list[str]]
+Path = list[tuple[int, int]]
+
+def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
+    pass
+
+def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> list[Path]:
+    pass
+
+def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> list[Path]:
+    pass
+
+def max_score_paths(board: Board, words: Iterable[str]) -> list[Path]:
+    pass
